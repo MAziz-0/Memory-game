f3e3f208fe73a9de62b42c893000a7806fce0a9e
MAziz-0 <maastudentacc@gmail.com>
2021-04-07 08:41:02 +0000

    Assigned variables which will be utilised in later functions

--- javascript/script.js
+++ javascript/script.js
@@ -4,13 +4,20 @@
 $(document).ready(function() {
     if (navigator.userAgent.search("Safari") >= 0 &&
     navigator.userAgent.search("Chrome") <0) {
-        $(".game").height($(window).height() * 0.9)
+        $(".game").height($(window).height() * 0.9);
     }
 }
 
-
-
-
 /* __________________________________________________ Variable assignment */
 
-var cards []
+var cards ['Gamakichi','Konoha','Ramen','Seal','Sharingan','Smile','Uchiha','Sasuke'];
+var cardPair = cards.concat(cards); 
+var cardSelect = [];
+var flipCards = [];
+var pairCount = 0;
+var startGame = false;
+var playing = false;
+var timeGiven = 30;
+var timeOut = false;
+var victoryRoyale = false;
+
