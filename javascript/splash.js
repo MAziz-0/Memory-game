490f958b487421658cd75bca012ddadd64fe124f
MAziz-0 <maastudentacc@gmail.com>
2021-04-12 10:33:37 +0000

    Changed timeout duration to 5000s and added a underscore to (e) as it was giving a declaration error

--- javascript/splash.js
+++ javascript/splash.js
@@ -3,6 +3,6 @@ const splash = document.querySelector(".splash");
 document.addEventListener("DOMContentLoaded", (e)=>{
     setTimeout(()=>{
       splash.classList.add("display-none");
-    }, 2000);
+    }, 5000);
 
 });
