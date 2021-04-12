ae451a29c14f6b703d37f0d08b274a9c821f6761
MAziz-0 <maastudentacc@gmail.com>
2021-04-12 17:31:41 +0000

    Created a timeout function which also hides the body when showing the splash screen

--- javascript/splash.js
+++ javascript/splash.js
@@ -1,8 +1,16 @@
 const splash = document.querySelector(".splash");
+const title = document.querySelector(".title-container");
+const timer = document.querySelector(".timer-container");
+const game = document.querySelector(".game");
+const body = document.querySelector(".game-body")
 
-document.addEventListener("DOMContentLoaded",(_e)=>{
+document.addEventListener("DOMContentLoaded",(e)=>{
     setTimeout(()=>{
       splash.classList.add("display-none");
-    }, 5000);
+      title.classList.remove("hidden");
+      timer.classList.remove("hidden");
+      game.classList.remove("hidden");
+      body.classList.add("background");
+    }, 2000);
 
 });
