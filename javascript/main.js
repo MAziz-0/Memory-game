067632e62f7dda07478ab3fd462ca3e00be6aef8
MAziz-0 <maastudentacc@gmail.com>
2021-04-12 17:33:27 +0000

    Changed name of script.js to main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,27 @@
+if (document.location.search.match(/type=embed/gi)) {
+    window.parent.postMessage("resize", "*");
+}
 
-/* __________________________________________________ Responsiveness on Safari/Chrome */
+/* ____________________________Responsiveness on Safari/Chrome */
+var $;
 
+$(document).ready(function () {
+    if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) {
+        $(".game").height($(window).height() * 0.9);
+    }
+
+    /* ___________________________ Variable assignment */
+
+    var cards = ["Gamakichi", "Konoha", "Ramen", "Seal", "Sharingan", "Smile", "Uchiha", "Sasuke"];
+    var cardPair = cards.concat(cards);
+    var cardSelect = [];
+    var flipCards = [];
+    var pairCount = 0;
+    var startGame = false;
+    var playing = false;
+    var timeGiven = 30;
+    var timeOut = false;
+    var victoryRoyale = false;
+    var startTimer = false;
 
     
