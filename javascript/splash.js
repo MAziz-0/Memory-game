0a3546fa96ceac57f234edbccac7959dd39615c8
MAziz-0 <maastudentacc@gmail.com>
2021-04-15 17:14:36 +0000

    Changed timeout duration and added comments to code

--- javascript/splash.js
+++ javascript/splash.js
@@ -1,9 +1,12 @@
+
+/* ____________________________Presents Splashscreen*/
+
 const splash = document.querySelector(".splash");
 const title = document.querySelector(".title-container");
 const timer = document.querySelector(".timer-container");
 const game = document.querySelector(".game");
-const body = document.querySelector(".game-body")
-
+const body = document.querySelector(".game-body");
+/* ____________________________Removes hidden classes from main game after set amount of time */
 document.addEventListener("DOMContentLoaded",(e)=>{
     setTimeout(()=>{
       splash.classList.add("display-none");
@@ -11,6 +14,6 @@ document.addEventListener("DOMContentLoaded",(e)=>{
       timer.classList.remove("hidden");
       game.classList.remove("hidden");
       body.classList.add("background");
-    }, 2000);
+    }, 8000);
 
 });
