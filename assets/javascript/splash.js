15e06d5056d3f2b2b496632fafc47eec3d8286ab
MAziz-0 <maastudentacc@gmail.com>
2021-04-19 13:09:26 +0000

    Moved JS folder to asset folder

---
+++ assets/javascript/splash.js
@@ -0,0 +1,21 @@
+
+/* ____________________________Presents tiu Splashscreen*/
+
+const splash = document.querySelector(".splash");
+const title = document.querySelector(".title-container");
+const timer = document.querySelector(".timer-container");
+const game = document.querySelector(".game");
+const body = document.querySelector(".game-body");
+const countdown = document.querySelector(".timer");
+/* ____________________________Removes hidden classes from main game after set amount of time */
+document.addEventListener("DOMContentLoaded",(_e)=>{
+    setTimeout(()=>{
+      splash.classList.add("display-none");
+      title.classList.remove("hidden");
+      timer.classList.remove("hidden");
+      game.classList.remove("hidden");
+      body.classList.add("background");
+      countdown.classList.remove("hidden");
+    }, 15000);
+
+});
